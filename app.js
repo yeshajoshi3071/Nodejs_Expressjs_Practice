f30f6455a7e0d424bb392bbe59e202f9faabf241
Yesha Joshi <joshi.ye@northeastern.edu>
2024-03-06 15:20:38 -0500

    Alternative syntax

--- app.js
+++ app.js
@@ -7,7 +7,7 @@
 const names = require('./4-names')
 console.log(names);
 const sayHi = require('./5-utils')
-
+const data = require('./6-alternative-flavor')
 sayHi('susan')
-sayHi(john)
-sayHi(peter)
+sayHi(names.john)
+sayHi(names.peter)
